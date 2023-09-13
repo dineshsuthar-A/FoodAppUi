949928e9691de27ea9fd0199254613c5668f8f40
Devang Pathak <devangpathak8@gmail.com>
2023-09-13 11:22:02 +0530

    Update staff-service.service.ts

--- src/app/Services/staff-service.service.ts
+++ src/app/Services/staff-service.service.ts
@@ -15,4 +15,35 @@ export class StaffServiceService {
   deleteStaff(id:any) {
     return this.http.delete(environment.apiUrl+"user/userid/"+id);
   }
+
+
+//ITEM SERVICES START
+
+getItem(foodOrderId: Number){
+  return this.http.get(`http://localhost:8080/item/${foodOrderId}`);
+}
+
+getOrderById(value: number) {
+  return this.http.get(`http://localhost:8080/foodorderbyid/${value}`);
+}
+
+saveFoodProduct(formData: any) {
+  return this.http.post(environment.apiUrl + "foodproduct/save",
+    formData);
+}
+
+//ITEM SERVICES END
+
+
+//EDIT ORDER SERVICE START
+
+getAllFoodOrder(value: number) {
+  return this.http.get(`http://localhost:8080/foodorder/${value}`);
+}
+
+updateStatus(foodOrder:any){
+  return this.http.put("http://localhost:8080/foodorder",foodOrder)
+}
+
+//EDIT ORDER SERVICE END
 }
